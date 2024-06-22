84482938ea960c041288664e7eec057b1ffc1cca
RcnonS <ratchanon.somprasong@gmail.com>
2024-06-22 15:10:02 +0700

    init project

---
+++ client/components/LimitedChar.jsx
@@ -0,0 +1,12 @@
+import "../src/css/LimitedChar.css";
+
+function LimitedChar({ text, limit }) {
+  const limitedText = text.length >= limit ? text.slice(0, limit) : text;
+  return (
+    <p className="description">
+      {limitedText} <span className="dotContinueRead">...</span>
+    </p>
+  );
+}
+
+export default LimitedChar;
